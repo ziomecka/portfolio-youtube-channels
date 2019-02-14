80e3fd42d8e4d09cf93953d66b471a4cbb07e6f1
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-02-14 11:39:39 +0100

    static index loadChannels and removeAllMediaQueryListeners imported and used

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,13 @@
 require( '../css/vendor.css' );
 require( '../sass/index.sass' );
+
+import {
+    LISTENERS_ID,
+    loadChannels,
+} from './channels/';
+
+import { removeAllMediaQueryListeners } from '@common';
+
+// IMPORTANT: If new channels loaded then firstly remove the media query listeners!
+removeAllMediaQueryListeners( { id: LISTENERS_ID } );
+loadChannels();

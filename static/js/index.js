dd9e6e0f9da5ba7840db215749e475bdff75b300
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-02-18 11:14:13 +0100

    common index manageMedia.addMediaListeners used for addin media listners. loadChannels, root passed in object

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,20 @@
 require( '../css/index.css' );
 
-import { CHANNELS_ROOT_SELECTOR } from './constants';
+import {
+    CHANNELS_ROOT_SELECTOR,
+    MEDIA,
+} from './constants';
 
 import {
     loadChannels,
 } from '@channels';
 
-loadChannels( CHANNELS_ROOT_SELECTOR );
+import {
+    manageMedia,
+} from '@common';
+
+// ADD MEDIA LISTENERS
+manageMedia.addMediaListeners({ media: MEDIA, window });
+
+// LOAD CHANNELS
+loadChannels( { root: CHANNELS_ROOT_SELECTOR } );

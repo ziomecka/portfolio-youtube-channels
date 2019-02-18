fe4ce341176bbf563f37db21dda0c8ac1196318b
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-02-18 11:15:32 +0100

    index.js filtering enabled index.html unnecessary code removed

--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,13 @@ require( '../css/index.css' );
 
 import {
     CHANNELS_ROOT_SELECTOR,
+    FILTER,
     MEDIA,
+    URLS,
 } from './constants';
 
 import {
+    enableFiltering,
     loadChannels,
 } from '@channels';
 
@@ -15,6 +18,32 @@ import {
 
 // ADD MEDIA LISTENERS
 manageMedia.addMediaListeners({ media: MEDIA, window });
+
+// ADD FILTERS
+const {
+    container: filterContainer,
+    id: filterId
+} = FILTER;
+
+const {
+    filterChannels: filterChannelsUrl,
+} = URLS;
+
+enableFiltering({
+    containerSelector: filterContainer,
+    field: 'title',
+    filterSelector: filterId,
+    root: CHANNELS_ROOT_SELECTOR,
+    url: filterChannelsUrl,
+});
+
+// enableFiltering({
+//     containerSelector: filterContainer,
+//     field: 'description',
+//     filterSelector: filterId,
+//     root: CHANNELS_ROOT_SELECTOR,
+//     url: filterChannelsUrl,
+// });
 
 // LOAD CHANNELS
 loadChannels( { root: CHANNELS_ROOT_SELECTOR } );

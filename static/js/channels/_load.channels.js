6b7ebc10748ec17196b83a96e812de578e856fd5
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-02-18 11:00:24 +0100

    channels load.channels changed. removeChannels option added. data commes either from api or from arguments check if root is provided removed - not needed - done by browser manageMedia used

--- static/js/channels/_load.channels.js
+++ static/js/channels/_load.channels.js
@@ -5,36 +5,57 @@ import {
 
 import {
     api,
-    removeAllMediaQueryListeners,
+    manageMedia,
 } from '@common';
 
-import createChannels from './_create.channels';
+import { createChannels } from './create/';
+import { removeChannels } from './remove/';
 
-async function _loadChannels ( root ) {
-    try {
-        createChannels( {
-            data: await api.get( URLS.channels ),
-            root,
-        } );
-    } catch ( err ) {
-        // TODO display error info
-        return undefined;
+async function _loadChannels ( options ) {
+    const { root } = options;
+    let { data } = options;
+
+    if ( !data ) {
+        try {
+            data = await api.get( { url: URLS.channels } );
+            createChannels( { data, root } );
+        } catch ( err ) {
+            /* eslint-disable no-console */
+            console.log( err );
+            /* eslint-enable no-console */
+            return undefined;
+            // TODO display some error message
+        }
+    } else {
+        createChannels( { data, root } );
     }
 }
 
-async function loadChannels ( root ) {
+/**
+ *
+ * @param {Object} options
+ * @param {string} options.root
+ * @param {object} options.data
+ * @param {boolean} options.removeChannels optional
+ */
+async function loadChannels ( options ) {
     if ( process.env.NODE_ENV !== 'production' ) {
-        if ( !root || ( typeof root !== 'string' ) ) {
+        const { data } = options;
+
+        if ( data && data.data && !Array.isArray( data.data ) ) {
             /* eslint-disable no-console */
-            console.warn( 'Root not provided' );
+            console.warn( 'loadChannels: data is not an array' );
             /* eslint-enable no-console */
             return undefined;
         }
     }
 
-    // IMPORTANT: If new channels loaded then firstly remove the media query listeners!
-    removeAllMediaQueryListeners( { id: LISTENERS_ID } );
-    _loadChannels( root );
+    if ( options.removeChannels ) {
+        manageMedia.unsubscribeFromMediaChanges( { id: LISTENERS_ID } );
+        removeChannels( options.root );
+    }
+
+    _loadChannels( options );
 }
 
 export default loadChannels;

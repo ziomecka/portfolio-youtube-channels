a79ae39a7bb6a6422d37056ca6d08b1b51566701
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-02-19 13:04:23 +0100

    common constants. Check is not 'production' added, so the code is removed by webpack in the production bundle.

--- static/js/common/constants.js
+++ static/js/common/constants.js
@@ -3,13 +3,17 @@ import {
     MEDIA,
 } from './media/constants';
 
-const COMMON_ERRORS = new Map( [
-    ['argument_not_string', 'Argument is not a string'],
-    ['argument_incorrect', 'Argument is incorrect'],
-    ['convert_numbers_not_number', 'Argument is not a stringified number'],
-    ['media_query_not_found', 'Listeners not found'],
-    ['media_query_not_function', 'Listener is not a function'],
-] );
+let COMMON_ERRORS;
+
+if ( process.env.NODE_ENV !== 'production' ) {
+    COMMON_ERRORS = new Map( [
+        ['argument_not_string', 'Argument is not a string'],
+        ['argument_incorrect', 'Argument is incorrect'],
+        ['convert_numbers_not_number', 'Argument is not a stringified number'],
+        ['media_query_not_found', 'Listeners not found'],
+        ['media_query_not_function', 'Listener is not a function'],
+    ] );
+}
 
 const VALID_ELEMENTS = [
     'a',

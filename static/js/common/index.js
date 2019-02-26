29beeae5422f3960fba2eaebe33846306180a27a
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-02-26 18:01:56 +0100

    common, index, all unnecessary exports removed.

--- static/js/common/index.js
+++ static/js/common/index.js
@@ -1,15 +1,4 @@
-export { MEDIA } from './constants';
-export { default as directionIcon } from './direction.icon/';
-export { default as manageDomListeners } from './dom.listeners';
-export { default as buildId } from './_build.id';
-export { default as api } from './api/';
-export { default as detectStorage } from './_detect.storage';
-export { default as helpers } from './_helpers';
-export { default as manageMedia } from './media/';
 export { default as convertNumbers } from './_convert.numbers';
-export { default as detectMedia } from './_detect.media';
-export { default as isValidUrl } from './_is.valid.url';
-export { default as manageDom } from './_manage.dom';
 export { default as createElement } from './_dom.create.element';
 export { default as getCheckedElement } from './_get.checked.element';
 export { default as manageDirectionIcon } from './direction.icon/';

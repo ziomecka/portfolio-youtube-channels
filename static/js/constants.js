5efb1be5750dc67b7c10c06bac8288ffef4ffe75
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-02-26 17:07:39 +0100

    index.js channelsEnableClear, channelsEnableFilter, channelsEnableSort, loadChannels used unnecessary const removed: CLEAR, FILTER, SORT

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,28 +1,11 @@
 const CHANNELS_ROOT_SELECTOR = '#fjs-main';
 
-const CLEAR = {
-    id: 'fjs-clear-button',
-};
-
-const FILTER = {
-    container: 'fjs-filter-container',
-    id: 'fjs-filter-text',
-};
-
 const LISTENERS_ID = 'channels-listeners';
-
-const SORT = {
-    container: 'fjs-sort-container',
-    id: 'fjs-sort-radio',
-};
 
 const SORT_ATTRIBUTE = 'data-sort';
 
 export {
     CHANNELS_ROOT_SELECTOR,
-    CLEAR,
-    FILTER,
     LISTENERS_ID,
-    SORT,
     SORT_ATTRIBUTE,
 };
--- static/js/index.js
+++ static/js/index.js
@@ -1,123 +1,17 @@
 require( '../css/index.css' );
 
 import {
-    CHANNELS_ROOT_SELECTOR,
-    CLEAR,
-    FILTER,
-    MEDIA,
-    SORT,
-    URLS,
-} from './constants';
+    channelsEnableClear,
+    channelsEnableFilter,
+    channelsEnableSort,
+} from '@channelsEvents';
 
-import {
-    detectStorage,
-    manageMedia,
-} from '@common';
+import loadChannels from '@channelsLoad';
 
-import {
-    enableClearing,
-    enableFiltering,
-    enableSorting,
-    loadChannels,
-} from '@channels';
+channelsEnableClear( '#clear', 'filter-title' );
 
-// DETECT LOCAL STORAGE
-const localStorageAvailable = detectStorage( 'localStorage' );
+channelsEnableFilter( 'title', '#filter-title' );
 
-// ADD MEDIA LISTENERS
-manageMedia.addMediaListeners( { media: MEDIA, window } );
+channelsEnableSort( '#fjs-sort-container' );
 
-const {
-    container: sortContainer,
-    id: sortId,
-} = SORT;
-
-// ADD FILTERS
-const {
-    container: filterContainer,
-    id: filterId,
-} = FILTER;
-
-const {
-    filterChannels: filterChannelsUrl,
-} = URLS;
-
-enableFiltering( {
-    containerSelector: filterContainer,
-    field: 'title',
-    filterSelector: filterId,
-    localStorageAvailable,
-    root: CHANNELS_ROOT_SELECTOR,
-    sortSelector: sortId,
-    url: filterChannelsUrl,
-} );
-
-// enableFiltering({
-//     containerSelector: filterContainer,
-//     field: 'description',
-//     filterSelector: filterId,
-//     root: CHANNELS_ROOT_SELECTOR,
-//     url: filterChannelsUrl,
-// });
-
-// ADD SORTERS
-const {
-    sortChannels: sortChannelsUrl,
-} = URLS;
-
-enableSorting( {
-    containerSelector: sortContainer,
-    direction: 'desc',
-    field: 'title',
-    label: 'title',
-    root: CHANNELS_ROOT_SELECTOR,
-    sortSelector: `${sortId}-title`,
-    url: sortChannelsUrl,
-} );
-
-enableSorting( {
-    containerSelector: sortContainer,
-    direction: 'desc',
-    field: 'statistics.subscriberCount',
-    label: 'subscribers',
-    root: CHANNELS_ROOT_SELECTOR,
-    sortSelector: `${sortId}-subscriber`,
-    url: sortChannelsUrl,
-} );
-
-enableSorting( {
-    containerSelector: sortContainer,
-    direction: 'desc',
-    field: 'statistics.videoCount',
-    label: 'videos',
-    root: CHANNELS_ROOT_SELECTOR,
-    sortSelector: `${sortId}-video`,
-    url: sortChannelsUrl,
-} );
-
-enableSorting( {
-    containerSelector: sortContainer,
-    direction: 'desc',
-    field: 'statistics.viewCount',
-    label: 'views',
-    root: CHANNELS_ROOT_SELECTOR,
-    sortSelector: `${sortId}-view`,
-    url: sortChannelsUrl,
-} );
-
-// ENABLE CLEARING
-const {
-    id: clearId,
-} = CLEAR;
-
-enableClearing( {
-    clearSelector: clearId,
-    filterSelector: filterId,
-    sortSelector: sortId,
-} );
-
-// LOAD CHANNELS
-loadChannels( {
-    localStorageAvailable,
-    root: CHANNELS_ROOT_SELECTOR,
-} );
+loadChannels();
